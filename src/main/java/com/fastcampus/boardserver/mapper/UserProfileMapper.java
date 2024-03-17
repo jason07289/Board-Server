35a961570970ac9eb41bd40b705ecf0573e051fd
jason07289 <jason07999@naver.com>
2024-03-17 19:24:34 +0900

    remove no usages

--- src/main/java/com/fastcampus/boardserver/mapper/UserProfileMapper.java
+++ src/main/java/com/fastcampus/boardserver/mapper/UserProfileMapper.java
@@ -9,10 +9,6 @@ import org.apache.ibatis.annotations.Param;
 public interface UserProfileMapper {
     public UserDTO getUserProfile(@Param("id") String id);
 
-    int insertUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
-
-    int updateUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
-
     int deleteUserProfile(@Param("id") String id);
 
     public int register(UserDTO userDTO);
@@ -21,11 +17,10 @@ public interface UserProfileMapper {
                                        @Param("password") String password);
 
     public UserDTO findByUserIdAndPassword(@Param("userId") String userId,
-                                       @Param("password") String password);
+                                       @Param("pa정ssword") String password);
 
     int idCheck(String id);
 
     public int updatePassword(UserDTO userDTO);
 
-    public int updateAddress(UserDTO userDTO);
 }
